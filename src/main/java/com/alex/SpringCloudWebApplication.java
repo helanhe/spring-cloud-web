042233c06662155b914d44028277d267ec3988e1
alex.yang <33939973@qq.com>
2020-10-29 10:35:29 +0800

    测试项目

--- src/main/java/com/alex/SpringCloudWebApplication.java
+++ src/main/java/com/alex/SpringCloudWebApplication.java
@@ -3,9 +3,16 @@ package com.alex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCloudWebApplication {
+public class SpringCloudWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String [] args) {
 
